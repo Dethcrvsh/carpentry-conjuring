3fb95bef43f4417b7016a41ad8a34d09b19f3424
eliasjohansson <eljoh633@student.liu.se>
2022-11-18 22:16:44 +0100

    added bulding and cursor

---
+++ BuidingMaster.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+onready var buildmap = $BuildingMap
+onready var cursormap = $CursorMap
+onready var buildobjs = $BuildingObjects
+
+var cursor_pos = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	update_cursor()
+
+func build_at(pos):
+	var build_pos = buildmap.world_to_map(pos)
+	buildmap.set_cell(build_pos.x, build_pos.y, 0)
+	
+func put_cursor(pos):
+	cursor_pos = cursormap.world_to_map(pos)
+
+func update_cursor():
+	for tile in cursormap.get_used_cells():
+		cursormap.set_cell(tile.x, tile.y, -1)
+	if cursor_pos != null:
+		cursormap.set_cell(cursor_pos.x, cursor_pos.y, 0)
+	
--- Player.gd
+++ Player.gd
@@ -2,8 +2,10 @@ extends KinematicBody2D
 
 var vel = Vector2.ZERO
 const speed = 200
-const acc = 30
-const decc = 50
+const acc = 60
+const decc = 40
+
+onready var build_master = get_parent().get_node("BuildingMaster")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,8 +14,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	do_player_movement()
+	do_bulding_input()
 
 func do_player_movement():
+	# Look at player input
 	var move_dir = Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
 		move_dir.y = -1
@@ -25,11 +29,23 @@ func do_player_movement():
 		move_dir.x = 1
 	# Deccelerate if no inputs
 	if move_dir.x == 0 and move_dir.y == 0:
+		# If speed is close to zero then make it zero
 		if vel.length() < decc:
 			vel = Vector2.ZERO
+		# slow down movement
 		else:
 			vel = vel - vel.normalized()*decc
+	# Add run_force according to input
 	else:
 		var run_force = move_dir.normalized()*acc
-		vel = (vel+run_force).clamped(speed)
+		vel = vel+run_force
+	# stop at top speed
+	vel = vel.clamped(speed)
 	move_and_slide(vel)
+
+func do_bulding_input():
+	var mous_pos = get_global_mouse_position()
+	build_master.put_cursor(mous_pos)
+	if Input.is_action_just_pressed("build"):
+		build_master.build_at(mous_pos)
+		
