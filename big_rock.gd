36b1ab9bbfb8952b51ecec36b0950f9308141fe6
eliasjohansson <eljoh633@student.liu.se>
2022-11-20 16:50:48 +0100

    coll for house

--- big_rock.gd
+++ big_rock.gd
@@ -13,6 +13,6 @@ func _ready():
 	var pos_3 = collmap.world_to_map(Vector2(self.position.x, self.position.y-buffer.y))
 	var pos_4 = collmap.world_to_map(Vector2(self.position.x-buffer.x, self.position.y))
 	for pos in [pos_1, pos_2, pos_3, pos_4]:
-		obmaster.add_collision(pos)
+		obmaster.add_collision(collmap.map_to_world(pos))
 
 
